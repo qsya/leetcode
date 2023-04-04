3d838ff6f380a6ad0fde442d2ad0a2db90d48585
qsya <1183757011@qq.com>
2023-04-04 14:42:44 +0800

    两数相加

--- daily/1.两数之和.js
+++ daily/1.两数之和.js
@@ -2,10 +2,12 @@
  * @Author: hexp 
  * @Date: 2023-04-03 14:08:19 
  * @Last Modified by: hexp
- * @Last Modified time: 2023-04-03 15:18:59
+ * @Last Modified time: 2023-04-04 10:02:50
  */
 
 /* 
+简单
+
 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
 
 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
