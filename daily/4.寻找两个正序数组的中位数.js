57b6381edf7a24e2e52176cab1bf3ebcc3aa5b0e
qsya <1183757011@qq.com>
2023-04-06 15:05:52 +0800

    寻找两个正序数组的中位数

--- daily/4.寻找两个正序数组的中位数.js
+++ daily/4.寻找两个正序数组的中位数.js
@@ -2,7 +2,7 @@
  * @Author: hexp 
  * @Date: 2023-04-05 22:29:42 
  * @Last Modified by: hexp
- * @Last Modified time: 2023-04-05 22:49:35
+ * @Last Modified time: 2023-04-06 14:33:18
  */
 
 /*
@@ -42,6 +42,63 @@ var findMedianSortedArrays = function (nums1, nums2) {
     return (arr[length / 2 - 1] + arr[length / 2]) / 2
   } else {
     return arr[Math.floor(length / 2)]
+  }
+}
+
+console.log(findMedianSortedArrays([3], [-2, -1]))
+
+// 解法二：双指针移动
+var findMedianSortedArrays = function (nums1, nums2) {
+  let m = nums1.length, n = nums2.length, left = -1, right = -1, aStart = 0, bStart = 0
+  for (let i = 0; i <= (m + n) / 2; i++) {
+    left = right
+    if (aStart < m && (bStart >= n || nums1[aStart] < nums2[bStart])) {
+      right = nums1[aStart++]
+    } else {
+      right = nums2[bStart++]
+    }
+  }
+  if ((m + n) & 1 === 0) return (left + right) / 2
+  else return right
+}
+
+console.log(findMedianSortedArrays([3], [-2, -1]))
+
+// 解法三：二分法 第k小数
+var findMedianSortedArrays = function (nums1, nums2) {
+  const len1 = nums1.length
+  const len2 = nums2.length
+  const mid = (len1 + len2 + 1) >> 1
+  const left = getK(nums1, nums2, 0, 0, mid)
+  // 判断总长度奇偶
+  if ((len1 + len2) % 2) {
+    return left
+  } else {
+    const right = getK(nums1, nums2, 0, 0, mid + 1)
+    return (left + right) / 2
+  }
+}
+
+// 取第k小的数
+function getK (nums1, nums2, start1, start2, k) {
+  const len1 = nums1.length
+  const len2 = nums2.length
+  /* 特例 */
+  // nums1 数组的元素排除完
+  if (len1 === start1) return nums2[start2 + k - 1]
+  if (len2 === start2) return nums1[start1 + k - 1]
+  // 排除到只剩两个元素取最小 即剩余元素的最小值
+  if (k === 1) return Math.min(nums1[start1], nums2[start2])
+
+  /* 通常情况 */
+  // 取k的一半 同时注意可能会超出数组长度 最多取数组最后一个元素
+  const i = start1 + Math.min(len1, k >> 1) - 1
+  const j = start2 + Math.min(len2, k >> 1) - 1
+  // j 前面的所有元素被排除了 同时缩减k的值
+  if (nums1[i] > nums2[j]) {
+    return getK(nums1, nums2, start1, j + 1, k - (j - start2 + 1))
+  } else {
+    return getK(nums1, nums2, i + 1, start2, k - (i - start1 + 1))
   }
 }
 
---
+++ other/2.二分查找.js
@@ -0,0 +1,50 @@
+/*
+ * @Author: hexp 
+ * @Date: 2023-04-06 09:55:56 
+ * @Last Modified by: hexp
+ * @Last Modified time: 2023-04-06 10:41:51
+ */
+
+/* 
+简单
+
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+
+示例 1:
+
+输入: nums = [-1,0,3,5,9,12], target = 9
+输出: 4
+解释: 9 出现在 nums 中并且下标为 4
+示例 2:
+
+输入: nums = [-1,0,3,5,9,12], target = 2
+输出: -1
+解释: 2 不存在 nums 中因此返回 -1
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/binary-search
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let low = 0, high = nums.length - 1
+  while (low <= high) {
+    const mid = Math.floor((high - low) / 2) + low
+    if (nums[mid] === target) {
+      return mid
+    } else if (nums[mid] < target) {
+      low = mid + 1
+    } else {
+      high = mid - 1
+    }
+  }
+  return -1
+}
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9))
